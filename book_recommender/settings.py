acb181cb084548e425178022c8f6664a23e84dbb
Avadhut Tanugade <avdhuttanugade55@gmail.com>
2021-06-11 21:35:33 +0530

    deployment issue fixed

--- book_recommender/settings.py
+++ book_recommender/settings.py
@@ -153,4 +153,4 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
