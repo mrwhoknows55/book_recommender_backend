b91d2fd08529504bf2721d7f217a666a5319305b
Avadhut Tanugade <avdhuttanugade55@gmail.com>
2021-06-30 00:56:48 +0530

    added user library get and post end points

--- users/models.py
+++ users/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from books.models import Book
 
 
 class User(AbstractUser):
     name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
+    library = models.ManyToManyField(Book, related_name="user")
     username = None
 
     USERNAME_FIELD = 'email'
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import SignUpView, LoginView, UserView, LogoutView
+from .views import SignUpView, LoginView, UserView, LogoutView, PostLibraryView, GetLibraryView
 
 urlpatterns = [
     path('signup', SignUpView.as_view()),
     path('login', LoginView.as_view()),
     path('user', UserView.as_view()),
-    path('logout', LogoutView.as_view())
+    path('logout', LogoutView.as_view()),
+    path('library/', GetLibraryView.as_view()),
+    path('library/<int:pk>', PostLibraryView.as_view()),
+    path('library/<int:pk>/', PostLibraryView.as_view())
 ]
